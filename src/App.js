bc72382836aa7d9bcda74cabcfde8cc2947f04dd
deepanwar <deepakumar6463@gamil.com>
2022-09-04 22:01:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
